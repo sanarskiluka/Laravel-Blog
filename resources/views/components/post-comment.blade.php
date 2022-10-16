ff4c50704964653077d8e363900a411df548f20b
sanarskiluka <sanarskiluka2003@gmail.com>
2022-10-17 00:22:26 +0400

    created component for repeating style

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,19 +1,21 @@
 @props(['comment'])
 
-<article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
-  <div class="flex-shrink-0">
-    <img src="https://i.pravatar.cc/100?u={{ $comment->id }}" alt="avatar" width="64px" height="64px" class="rounded-xl">
-  </div>
+<x-panel class="bg-gray-50">
+  <article class="flex space-x-4">
+    <div class="flex-shrink-0">
+      <img src="https://i.pravatar.cc/100?u={{ $comment->id }}" alt="avatar" width="64px" height="64px" class="rounded-xl">
+    </div>
 
-  <div>
-    <header class="mb-4">
-      <h3 class="font-bold">{{ $comment->author->name }}</h3>
-      <p class="text-xs">
-        Posted <time>{{ $comment->created_at->diffForHumans() }}</time>
-      </p>
-    </header>
+    <div>
+      <header class="mb-4">
+        <h3 class="font-bold">{{ $comment->author->name }}</h3>
+        <p class="text-xs">
+          Posted <time>{{ $comment->created_at->diffForHumans() }}</time>
+        </p>
+      </header>
 
-    <p>{{ $comment->body }}</p>
+      <p>{{ $comment->body }}</p>
 
-  </div>
-</article>
+    </div>
+  </article>
+</x-panel>
